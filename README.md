6ee2f4e8a38dbde79126468ff9564dcfbcbed521
hongguo <play4fun@foxmail.com>
2019-04-13 20:05:43 +0800

    fix

--- README.md
+++ README.md
@@ -27,7 +27,7 @@
 - 从淘宝下载的top，python是2.7的，满是bug
     - 现在已经修改为python 3.6
 - Enjoy！
-    - 有一些api没有加入，因为下载sdk时，淘宝是根据你的权限自动生成的。很讨厌！
+    - 有一些api没有加入，因为下载sdk时，淘宝是根据你的权限自动生成的。
     - 有时候新增加了一些api，也不知道
 - 安装
     - python setup.py clean 
--- top/api/rest/TbkTpwdCreateRequest.py
+++ top/api/rest/TbkTpwdCreateRequest.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 '''
-Created by auto_sdk on 2018.04.04
 
 http://open.taobao.com/docs/api.htm?apiId=31127
 '''
@@ -11,9 +10,11 @@ class TbkTpwdCreateRequest(RestApi):
     def __init__(self, domain='gw.api.taobao.com', port=80):
         RestApi.__init__(self, domain, port)
         self.ext = None
+
         self.logo = None
         self.text = None
         self.url = None
+
         self.user_id = None
 
     def getapiname(self):
