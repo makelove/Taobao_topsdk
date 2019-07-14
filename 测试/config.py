f8eb3815eed1791f237abf0f71907184ba9ba516
play4fun <play4fun@foxmail.com>
2019-07-14 14:11:49 +0800

    add config.py

---
+++ 测试/config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+config.py:
+"""
+
+
+#设置为你的key
+appkey = "123456"
+secret = "fjslkdjflkfjslkdjflkfjslkdjflk"
+pid = 274398237498  # mm_123_456_274398237498 #最后一位
+
